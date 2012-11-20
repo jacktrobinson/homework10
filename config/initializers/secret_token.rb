0abb8917ae2dd4b25cc25ce83be6d7604cf746e0
Jack Robinson <jrobins@wharton.upenn.edu>
2012-11-20 13:25:58 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hw10::Application.config.secret_token = 'bee957fa0fd5dfece65b95343aec3e8077730e7e044e02321cdd5e155a508c49f570a9f39607004bace3c183a573494e6a3540ab49e1f226ef39cb4f7b5cab1d'
